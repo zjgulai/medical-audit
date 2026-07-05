f8026730d2e02b58cbf7deeb6ca37d97f95b8869
zjgulai <zjgulai@github.com>
2026-07-06 03:43:08 +0800

    fix: route production root to login

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,5 @@
-import { ProjectDashboard } from "@/components/dashboard/project-dashboard";
-import { WorkspaceShell } from "@/components/shell/workspace-shell";
-import { currentSelfCheckProject } from "@/lib/projects";
+import LoginPage from "./login/page";
 
 export default function HomePage() {
-  return (
-    <WorkspaceShell>
-      <ProjectDashboard project={currentSelfCheckProject} />
-    </WorkspaceShell>
-  );
+  return <LoginPage />;
 }
